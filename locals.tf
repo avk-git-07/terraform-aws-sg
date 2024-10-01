a34f769e823935e751974c1c39b184e92e2bb8af
avk-git-07 <asurivamshi@gmail.com>
2024-10-01 07:11:42 +0530

    terraform-aws-sg

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  sg_full_name = "${var.proj_name}-${var.env}-${var.inst_type}"
+
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "proj_name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "inst_type" {
+  type = string
+}
+
+variable "common_tags" {
+  default = {}
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
+variable "vpc_id1" {
+  type = string
+}
+
+
